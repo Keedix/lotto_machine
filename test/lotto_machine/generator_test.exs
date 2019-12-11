fadda7a9a01f1ddeb3d7699813bfcd8e7f3d02b1
auklejewski <artur.uklejewski@gmail.com>
2019-12-11 20:54:22 +0100

    Fix failing tests

--- test/lotto_machine/generator_test.exs
+++ test/lotto_machine/generator_test.exs
@@ -1,23 +1,15 @@
 defmodule LottoMachine.GeneratorTest do
   use ExUnit.Case, async: true
 
+  alias LottoMachine.Generator.{Lotto, MultiMulti}
+
   test "should return 6 random numbers - lotto game" do
-    actual = LottoMachine.Generator.generate(:lotto)
-
-    assert length(actual) == 6
-
-    actual = LottoMachine.Generator.generate("lotto")
-
+    actual = Lotto.generate()
     assert length(actual) == 6
   end
 
   test "should return 10 random numbers - mutli multi game" do
-    actual = LottoMachine.Generator.generate(:multi_multi)
-
-    assert length(actual) == 10
-
-    actual = LottoMachine.Generator.generate("multi_multi")
-
+    actual = MultiMulti.generate()
     assert length(actual) == 10
   end
 end
